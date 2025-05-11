2bbab63074993853fb613eb9d4ffe3d91279601c
mihxw <mishamishavetrov@gmail.com>
2025-05-11 22:05:58 +0300

    f

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -12,11 +12,8 @@ export const initMongoConnection = async () => {
         // Вона вказується після MONGODB_URL між "/" і "?"
         try {
         await mongoose.connect(
-            //mongodb+srv://dianazinevych02:<db_password>@cluster0.amo7ris.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
             `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
         );
-        //як аргумент передамо рядок з посиланням для підключення (connection string). 
-        // Для формування цього посилання ми використаємо утилітарну функцію env, яка забезпечує доступ до змінних оточення.
         console.log('Mongo connection successfully established!');
 
     } catch (e) {
