fd8e15b390c66324a171ad083b592487eebc1f69
mihxw <mishamishavetrov@gmail.com>
2025-05-16 19:20:43 +0300

    f

--- src/models/contacts.js
+++ src/models/contacts.js
@@ -1,25 +1,12 @@
 import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  }
-}, { versionKey: false, timestamps: true });
+  name: String,
+  email: String,
+  phone: String,
+  favorite: Boolean,
+});
 
 const Contact = mongoose.model("Contact", contactSchema);
 
-export default Contact;
+export default Contact;  // <--- саме default export
